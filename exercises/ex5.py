0bb5ce6a9263985d42cc55af0a8f0b928d0b7770
Murilo <murilo_yanq6517@hotmail.com>
2023-03-08 15:48:26 -0300

    add ex5.py

---
+++ exercises/ex5.py
@@ -0,0 +1,9 @@
+
+primeiro_valor = input("Digite um valor: ")
+segundo_valor = input("Digite outro valor: ")
+
+if primeiro_valor > segundo_valor:
+    print(f"primeiro_valor=\"{primeiro_valor}\" é maior que segundo_valor=\"{segundo_valor}\"")
+else: 
+    print(f"primeiro_valor=\"{primeiro_valor}\" é menor que segundo_valor=\"{segundo_valor}\"")
+    
